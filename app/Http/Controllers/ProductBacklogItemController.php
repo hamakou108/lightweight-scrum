7231c5ce431b1d87fa9900c57e569b5fd48eb772
hamakou108 <hamakou108@gmail.com>
2020-02-02 21:20:48 +0900

    feature: Add api to add product backlog items

--- app/Http/Controllers/ProductBacklogItemController.php
+++ app/Http/Controllers/ProductBacklogItemController.php
@@ -4,10 +4,23 @@ namespace App\Http\Controllers;
 
 use App\ProductBacklogItem;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class ProductBacklogItemController extends BaseController
 {
+    public function store(Request $request) {
+        $pbi = new ProductBacklogItem();
+        $pbi->name = $request->name;
+
+        DB::transaction(function() use ($pbi) {
+            $pbi->save();
+        });
+
+        return response($pbi->jsonSerialize(), Response::HTTP_CREATED);
+    }
+
     public function index() {
         return response(ProductBacklogItem::all()->jsonSerialize(), Response::HTTP_OK);
     }
